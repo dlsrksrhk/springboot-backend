40565c4f734222001b7f3832031dc99ccfcb4711
sweet.dh <dlsrksrhk@gmail.com>
2024-05-31 19:51:29 +0900

    초기 세팅

---
+++ src/main/java/me/kdh/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package me.kdh.repository;
+
+import me.kdh.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByName(String name);
+
+    @Query("select m from Member m where m.name = ?1")
+    Optional<Member> findByNameQuery(String name);
+}
